1a95debb3028a43d879e80087254b775922c1bd1
DillonBorden <dillondaughtry2012@gmail.com>
2024-01-05 22:28:35 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+
+function writeCards(names, event){
+    
+    let messages = [];
+    for(let i = 0; i < names.length ; i++){
+        let message = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+        messages.push(message);
+        
+
+    }
+    return messages;
+    
+}
+
+function countDown(countdownNum){
+    while(countdownNum >= 0){
+        console.log(countdownNum);
+        countdownNum--;
+        
+    }
+}
